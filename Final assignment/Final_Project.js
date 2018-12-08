4e533a449664251e4e41c726b54069fb7c3f9120
Tariq Alhindi <tariq@Tariqs-MacBook-Pro.local>
2018-12-08 17:42:00 -0500

    countries autocomplete first attempt

--- Final assignment/Final_Project.js
+++ Final assignment/Final_Project.js
@@ -94,6 +94,31 @@ d3.csv("data/Land_Area.csv", function(data) {
     landarea.push(data)
 });
 
+// function country_match(){
+  // var validCounries = ["Algeria","Angola","Benin","Botswana","Burkina Faso","Burundi","Cameroon","Cape Verde","Central Arfrican Republic","Chad","Comoros","Democratic Republic of Congo","Congo","Cote D Ivoire","Djibouti","Egypt","El Salvador","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea Bissau","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Nauro","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"];
+  // for (let i=0; i<electricity_data.length; i++){
+  //   validCounries.push(electricity_data[i]["Country_Name"])
+  // }
+  // previousValue = "";
+  
+  // $('input#country_textbox').autocomplete({
+  //     autoFocus: true,
+  //     source: validOptions
+  // }).keyup(function() {
+  //     var isValid = false;
+  //     for (i in validOptions) {
+  //         if (validOptions[i].toLowerCase().match(this.value.toLowerCase())) {
+  //             isValid = true;
+  //         }
+  //     }
+  //     if (!isValid) {
+  //         this.value = previousValue
+  //     } else {
+  //         previousValue = this.value;
+  //     }
+  // });
+// }
+
 function electricity_tab() {
   var elements = document.getElementsByClassName("education-tab");
 //   console.log(elements)
@@ -246,8 +271,6 @@ function submit_arrow() {
   var popyear2 = parseInt(country_population[year2])
   var popyear1_d = "M1367.2,445.9v18l13,13h"+(popyear1/Math.max(popyear1,popyear2)*200)+"v-18l-13,-13Z" 
   var popyear2_d = "M1367.2,485.2v18l13,13h"+(popyear2/Math.max(popyear1,popyear2)*200)+"v-18l-13,-13Z"
-  console.log(popyear1/Math.max(popyear1,popyear2)*200)
-  console.log(popyear2/Math.max(popyear1,popyear2)*200)
   
   document.getElementById("population_year1").innerHTML = year1
   document.getElementById("population_year1_value").innerHTML = popyear1.toLocaleString()
@@ -255,8 +278,6 @@ function submit_arrow() {
   document.getElementById("population_year2").innerHTML = year2
   document.getElementById("population_year2_value").innerHTML = popyear2.toLocaleString()
   document.getElementById("population_box_year2").setAttribute('d', popyear2_d)
-  console.log(document.getElementById("population_box_year1"))
-  console.log(document.getElementById("population_box_year2"))
 
   document.getElementById("landarea").innerHTML = parseInt(country_landarea[2017]).toLocaleString()
   document.getElementById("map").style.display = "none"
@@ -267,7 +288,6 @@ function submit_arrow() {
   for (let i=0; i<elements.length; i++){
     elements[i].style.display = "block"
   }
-
 
 }
 
@@ -294,3 +314,115 @@ function close_button(){
   electricity_tab()
 
 }
+
+
+
+// <!--Make sure the form has the autocomplete function switched off:-->
+// <form autocomplete="off" action="/action_page.php">
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var countries = ["Algeria","Angola","Benin","Botswana","Burkina Faso","Burundi","Cameroon","Cape Verde","Central Arfrican Republic","Chad","Comoros","Democratic Republic of Congo","Congo","Cote D Ivoire","Djibouti","Egypt","El Salvador","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea Bissau","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Nauro","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"];
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+// window.onload=function(){
+//     autocomplete(document.getElementById("country_textbox"), countries);
+// }
+
+
